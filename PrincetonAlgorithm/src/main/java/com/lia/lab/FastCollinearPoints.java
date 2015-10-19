bbe439136ba7bce70a1018a619acae53a9a487dc
anotherdaylia <anotherdaylia@gmail.com>
2015-10-18 17:14:11 -0700

    FastCollinearPoints.java fixed loop bugs

--- PrincetonAlgorithm/src/main/java/com/lia/lab/FastCollinearPoints.java
+++ PrincetonAlgorithm/src/main/java/com/lia/lab/FastCollinearPoints.java
@@ -46,42 +46,55 @@ public class FastCollinearPoints {
                 }else if (points[p].slopeTo(points[q - 1]) == points[p].slopeTo(points[q])) {
                     pointList.add(points[q]);
                 }else if(pointList.size() > 2) {
-                    pointList.add(points[p]);
-                    Collections.sort(pointList, Point.pointOrder());
+                    check(pointList, p, map);
 
-                    Double slope = pointList.get(0).slopeTo(pointList.get(pointList.size()-1));
-
-                    if (map.containsKey(slope)) {
-                        Point ls_p = map.get(slope).getP();
-                        Point ls_q = map.get(slope).getQ();
-
-                        if (pointList.get(0).compareTo(ls_p) < 0) {
-                            ls_p = pointList.get(0);
-                        }
-
-                        if (pointList.get(pointList.size()-1).compareTo(ls_q) > 0) {
-                            ls_q = pointList.get(pointList.size()-1);
-                        }
-
-                        map.put(slope, new LineSegment(ls_p, ls_q));
-
-                    } else {
-                        LineSegment ls = new LineSegment(pointList.get(0), pointList.get(pointList.size()-1));
-                        map.put(slope, ls);
-                        numberOfSegments++;
-                    }
+                    pointList.clear();
+                    pointList.add(points[q]);
 
                 }else {
                     pointList.clear();
                     pointList.add(points[q]);
                 }
             }
+
+            if (pointList.size() > 2) {
+                check(pointList, p, map);
+            }
+
         }
 
         LineSegment[] lineSgmtArr = new LineSegment[map.size()];
         lineSgmtArr = map.values().toArray(lineSgmtArr);
 
         return lineSgmtArr;
+    }
+
+
+    private void check(ArrayList<Point> pointList, int p, HashMap<Double, LineSegment> map) {
+        pointList.add(points[p]);
+        Collections.sort(pointList, Point.pointOrder());
+
+        Double slope = pointList.get(0).slopeTo(pointList.get(pointList.size()-1));
+
+        if (map.containsKey(slope)) {
+            Point ls_p = map.get(slope).getP();
+            Point ls_q = map.get(slope).getQ();
+
+            if (pointList.get(0).compareTo(ls_p) < 0) {
+                ls_p = pointList.get(0);
+            }
+
+            if (pointList.get(pointList.size()-1).compareTo(ls_q) > 0) {
+                ls_q = pointList.get(pointList.size()-1);
+            }
+
+            map.put(slope, new LineSegment(ls_p, ls_q));
+
+        } else {
+            LineSegment ls = new LineSegment(pointList.get(0), pointList.get(pointList.size()-1));
+            map.put(slope, ls);
+            numberOfSegments++;
+        }
     }
 
 }
