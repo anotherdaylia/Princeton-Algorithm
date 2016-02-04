a7a8cc7f9bae3b306eed69f98d9baa3d230233cc
anotherdaylia <anotherdaylia@gmail.com>
2016-02-04 11:18:26 -0800

    fix bugs for bottom row connected before full issue.

--- PrincetonAlgorithm/src/main/java/com/lia/lab/Percolation/Percolation.java
+++ PrincetonAlgorithm/src/main/java/com/lia/lab/Percolation/Percolation.java
@@ -41,29 +41,44 @@ public class Percolation {
         // 1. open site (i, j)
         sites[ix] = true;
 
-        // 2. union site to topsite if it's on the 1st row
-        if ( isTopSite(ix) ) { unionUF.union(ix, topSite);}
+        // 2. union site to top site if it's on the 1st row
+        if ( isTopSite( ix ) ) { unionUF.union(ix, topSite); }
 
-        // 3. union site to bottomsite if it's on the last row
-        if ( isBottomSite(ix) ) { unionUF.union(ix, bottomSite);}
-
-        // 4. union to the site's open neighbors
-        if ( i - 1 > 0 && isOpen( i - 1, j )) { // up
-            unionUF.union(ix, getIndex(i - 2, j - 1)); // -1 when getting 1d index
+        // 3. union to the site's open neighbors
+        boolean hasNeighbor = false;
+        if ( i > 1 && isOpen( i - 1, j ) ) { // up
+            hasNeighbor = true;
+            unionUF.union( ix, getIndex(i - 2, j - 1) ); // -1 when getting 1d index
         }
 
-        if ( i + 1 < N && isOpen( i + 1, j )) { // down
-            unionUF.union(ix, getIndex( i, j - 1));
+        if ( i < N && isOpen( i + 1, j ) ) { // down
+            hasNeighbor = true;
+            unionUF.union( ix, getIndex( i, j - 1) );
         }
 
-        if ( j - 1 > 0 && isOpen( i, j - 1 )) { // left
-            unionUF.union(ix, getIndex(i - 1, j - 2));
+        if ( j > 1 && isOpen( i, j - 1 ) ) { // left
+            hasNeighbor = true;
+            unionUF.union( ix, getIndex(i - 1, j - 2) );
         }
 
-        if ( j + 1 < N && isOpen( i, j + 1 )) { // right
-            unionUF.union(ix, getIndex(i - 1, j));
+        if ( j < N && isOpen( i, j + 1 ) ) { // right
+            hasNeighbor = true;
+            unionUF.union( ix, getIndex(i - 1, j) );
         }
 
+        // 4. union site to bottomsite if it's connected to the top site!!
+        if ( hasNeighbor ) {
+            updateBottom();
+        }
+    }
+
+    // union full site on the lst row to bottom site.
+    private void updateBottom () {
+        for ( int k = 1; k <= N; k++ ) {
+            if ( isFull(N, k) ) {
+                unionUF.union( getIndex(N-1,k-1), bottomSite );
+            }
+        }
     }
 
     private void checkInput ( int i, int j ) {
@@ -105,11 +120,11 @@ public class Percolation {
     }
 
     private boolean isTopSite(int ix) {
-        return ix <= N;
+        return ix < N;
     }
 
     private boolean isBottomSite(int ix) {
-        return ix >= (N - 1) * N + 1;
+        return ix >= (N - 1) * N;
     }
 
     // test client (optional)
